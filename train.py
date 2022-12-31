4d8064786b2562a674c81f7759b2fabdf2e9594f
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-12-31 00:40:10 -0800

    Update train.py

--- train.py
+++ train.py
@@ -26,3 +26,42 @@ def construct_conv(row, tokenizer, eos = True):
 
 def load_and_cache_examples(args, tokenizer, df_trn):
     return ConversationDataset(tokenizer, args, df_trn)
+
+def set_seed(args):
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+class ConversationDataset(Dataset):
+    def __init__(self, tokenizer: PreTrainedTokenizer, args, df, block_size=512):
+
+        block_size = block_size - (tokenizer.model_max_length - tokenizer.max_len_single_sentence)
+        directory = args.cache_dir
+        cached_features_file = os.path.join(directory, args.model_type + "_cached_lm_" + str(block_size))
+
+        logger.info("Creating features from dataset file at %s", directory)
+        self.examples = []
+        for _, row in df.iterrows():
+            conv = construct_conv(row, tokenizer)
+            self.examples.append(conv)
+
+        logger.info("Saving features into cached file %s", cached_features_file)
+        with open(cached_features_file, "wb") as handle:
+            pickle.dump(self.examples, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, item):
+        return torch.tensor(self.examples[item], dtype=torch.long)
+
+def train(args, train_dataset, model: PreTrainedModel, tokenizer: PreTrainedTokenizer) -> Tuple[int, float]:
+    if args.local_rank in [-1, 0]:
+        tb_writer = SummaryWriter()
+
+    args.train_batch_size = args.per_gpu_train_batch_size * max(1, args.n_gpu)
+
+    def collate(examples: List[torch.Tensor]):
+        if tokenizer._pad_token is None:
+            return pad_sequence(examples, batch_first=True)
+        return pad_sequence(examples, batch_first=True, padding_value=tokenizer.pad_token_id
