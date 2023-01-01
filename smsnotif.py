9feadf20d586fcd70322b1ecc498b0c6e98b3f19
Birb12 <53247327+Birb12@users.noreply.github.com>
2023-01-01 05:47:45 -0800

    Update smsnotif.py

--- smsnotif.py
+++ smsnotif.py
@@ -2,6 +2,10 @@ import os
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 from flask import Flask, request, redirect
+import datetime
+import time
+import calendar
+
 
 
 account_sid = ""
@@ -17,3 +21,8 @@ def run_notify(resolutions, phonenum):
     message = client.messages.create(body=body, messaging_service_sid='MGf681960bb091aa4c9262ed305d69b82d', to=phonenum)
     print(message.sid)
 
+def running_all_the_time(): # for le cloud of course
+    while True:
+        today = datetime.date.today()
+        if today.day == calendar.month_range(today.year, today.month):
+            run_notify()
